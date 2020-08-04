71465180f90ffc46ba30ac3b2d118053f491c65d
Sergei Kononov <fillyjonk@protonmail.com>
2020-08-05 01:15:55 +0200

    type: feat

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <cpprest/filestream.h>
 #include <cpprest/http_client.h>
 
+#include <memory>
+#include <string>
+
+// namespaces used by CasaBlanca (CPPRESTSDK)
 using namespace utility;               // Common utilities like string conversions
 using namespace web;                   // Common features like URIs.
 using namespace web::http;             // Common HTTP functionality
 using namespace web::http::client;     // HTTP client features
 using namespace concurrency::streams;  // Asynchronous streams
 
-enum ApiData { time, states };
+// enums to retrieve data from api calls
+enum ApiData { time_now, states };
 enum States {
   icao24,
   callsign,
@@ -28,19 +33,24 @@ enum States {
   position_source
 };
 
+// api details
+std::string domain{"https://opensky-network.org/"};
+std::string path{"/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226"};
+
+
 int main(int argc, char* argv[]) {
-  auto fileStream = std::make_shared<ostream>();
+  std::shared_ptr<ostream> fileStream = std::make_shared<ostream>();
 
   // Open stream to output file.
   pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile) {
     *fileStream = outFile;
 
     // Create http_client to send the request.
-    http_client client(U("http://www.bing.com/"));
+    http_client client(U(domain));
 
     // Build request URI and start the request.
-    uri_builder builder(U("/search"));
-    builder.append_query(U("q"), U("cpprestsdk github"));
+    uri_builder builder(U(path));
+    // builder.append_query(U("q"), U("cpprestsdk github"));
     return client.request(methods::GET, builder.to_string());
   })
 
