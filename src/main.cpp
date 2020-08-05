acb627b22699de3a6f04d2d6971af5981f2a40e1
Sergei Kononov <fillyjonk@protonmail.com>
2020-08-06 00:40:56 +0200

    feat: Added Parser namespace, created Aeroplane class.

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,33 @@ using namespace web::http;             // Common HTTP functionality
 using namespace web::http::client;     // HTTP client features
 using namespace concurrency::streams;  // Asynchronous streams
 
-// enums to parse OpenSky data from api calls
-enum ApiData { time_now, states };
-enum States {
-  icao24,  // Unique ICAO 24-bit address of the transponder in hex string representation.
-  callsign,  // Callsign of the vehicle (8 chars). Can be null if no callsign has been received.
-  origin_country, // Country name inferred from the ICAO 24-bit address.
-  time_position,  // Unix timestamp (seconds) for the last position update. Can be null if no position report was received by OpenSky within the past 15s.
-  last_contact,  // Unix timestamp (seconds) for the last update in general. This field is updated for any new, valid message received from the transponder.
-  longitude,  // WGS-84 longitude in decimal degrees. Can be null.
-  latitude,  // WGS-84 latitude in decimal degrees. Can be null.
-  baro_altitude,  // Barometric altitude in meters. Can be null.
-  on_ground,  // Boolean value which indicates if the position was retrieved from a surface position report.
-  velocity,  // Velocity over ground in m/s. Can be null.
-  true_track,  // True track in decimal degrees clockwise from north (north=0°). Can be null.
-  vertical_rate,  // Vertical rate in m/s. A positive value indicates that the airplane is climbing, a negative value indicates that it descends. Can be null.
-  sensors,  // IDs of the receivers which contributed to this state vector. Is null if no filtering for sensor was used in the request.
-  geo_altitude,  // Geometric altitude in meters. Can be null.
-  squawk,  // The transponder code aka Squawk. Can be null.
-  spi,  // Whether flight status indicates special purpose indicator.
-  position_source  // Origin of this state’s position: 0 = ADS-B, 1 = ASTERIX, 2 = MLAT
-};
+// FILE: PARSER.CPP
+namespace Parser {
+  // enums to parse OpenSky data from api calls
+  enum ApiData { time_now, states };
+  enum States {
+    icao24,  // Unique ICAO 24-bit address of the transponder in hex string representation.
+    callsign,  // Callsign of the vehicle (8 chars). Can be null if no callsign has been received.
+    origin_country, // Country name inferred from the ICAO 24-bit address.
+    time_position,  // Unix timestamp (seconds) for the last position update. Can be null if no position report was received by OpenSky within the past 15s.
+    last_contact,  // Unix timestamp (seconds) for the last update in general. This field is updated for any new, valid message received from the transponder.
+    longitude,  // WGS-84 longitude in decimal degrees. Can be null.
+    latitude,  // WGS-84 latitude in decimal degrees. Can be null.
+    baro_altitude,  // Barometric altitude in meters. Can be null.
+    on_ground,  // Boolean value which indicates if the position was retrieved from a surface position report.
+    velocity,  // Velocity over ground in m/s. Can be null.
+    true_track,  // True track in decimal degrees clockwise from north (north=0°). Can be null.
+    vertical_rate,  // Vertical rate in m/s. A positive value indicates that the airplane is climbing, a negative value indicates that it descends. Can be null.
+    sensors,  // IDs of the receivers which contributed to this state vector. Is null if no filtering for sensor was used in the request.
+    geo_altitude,  // Geometric altitude in meters. Can be null.
+    squawk,  // The transponder code aka Squawk. Can be null.
+    spi,  // Whether flight status indicates special purpose indicator.
+    position_source  // Origin of this state’s position: 0 = ADS-B, 1 = ASTERIX, 2 = MLAT
+  };
+}
+
+
+// EOF PARSER.CPP
 
 // api details
 std::string domain{"https://opensky-network.org/"};
