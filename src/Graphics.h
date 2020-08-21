2d599631ced3e789884b7ba6751c13c9b883c9e6
Sergei Kononov <fillyjonk@protonmail.com>
2020-08-21 23:52:49 +0200

    refactor: Graphics rewritten.

--- src/Graphics.h
+++ src/Graphics.h
@@ -1,33 +1,35 @@
-// #ifndef GRAPHICS_H
-// #define GRAPHICS_H
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
 
-// #include <opencv2/core.hpp>
-// #include <string>
-// #include <vector>
+#include <opencv2/core.hpp>
+#include <string>
+#include <vector>
 
-// #include "TrafficObject.h"
+class Graphics {
+ public:
+  // constructor / desctructor
 
-// class Graphics {
-//  public:
-//   // constructor / desctructor
+  // getters / setters
+  // void SetBgFilename(std::string filename) {
+  //   _bgFilename = filename;
+  // }
+  // void SetTrafficObjects(std::vector<std::shared_ptr<TrafficObject>> &trafficObjects) {
+  //   _trafficObjects = trafficObjects;
+  // };
 
-//   // getters / setters
-//   void setBgFilename(std::string filename) { _bgFilename = filename; }
-//   void setTrafficObjects(std::vector<std::shared_ptr<TrafficObject>> &trafficObjects) { _trafficObjects = trafficObjects; };
+  // typical behaviour methods
+  void Simulate();
 
-//   // typical behaviour methods
-//   void simulate();
+ private:
+  // typical behaviour methods
+  void LoadBackgroundImg();
+  void DrawTrafficObjects();
 
-//  private:
-//   // typical behaviour methods
-//   void loadBackgroundImg();
-//   void drawTrafficObjects();
+  // member variables
+  // std::vector<std::shared_ptr<TrafficObject>> _trafficObjects;
+  std::string _bgFilename;
+  std::string _windowName;
+  std::vector<cv::UMat> _images;
+};
 
-//   // member variables
-//   std::vector<std::shared_ptr<TrafficObject>> _trafficObjects;
-//   std::string _bgFilename;
-//   std::string _windowName;
-//   std::vector<cv::UMat> _images;
-// };
-
-// #endif
+#endif
