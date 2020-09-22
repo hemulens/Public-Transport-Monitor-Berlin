ac10fb9fb3da71348e1daf394080f20bc31f0299
Sergei Kononov <fillyjonk@protonmail.com>
2020-09-22 21:23:25 +0200

    fix: Map files' sizes reduced.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -13,7 +13,7 @@
 int main() {
   // Draw all objects in vector
   Graphics *graphics = new Graphics();
-  graphics->SetBgFilename("../data/berlin-xl-4.png"); 
+  graphics->SetBgFilename("../data/berlin-xl-4.jpg"); 
   graphics->SetBgSize(2880, 1620);
   PublicTransport publicTransport;
   graphics->SetVehicles(publicTransport.GetVehiclesPtr());
